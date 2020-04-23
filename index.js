20f6b5dc04e8bde2be9dcc57436c6726f063d00c
zahidn <zahidullah4772@gmail.com>
2020-04-23 18:27:43 +0500

    Added new file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-console.log("Hello World");
+console.log("Hello and hi World");
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
