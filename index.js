0a6333a823d02329aecc0fa958555c7c24bf16b4
zahidn <zahidullah4772@gmail.com>
2020-04-23 18:36:00 +0500

    zahid added something

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-console.log("Hello and hi jani World");
+console.log("He llo and hi jani World");
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
 });
